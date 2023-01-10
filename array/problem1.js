5a5e73f06269e3da77841674814551f4df04bee9
shashikanttript <shashikantg712000@gmail.com>
2023-01-10 12:43:05 +0530

    first commit

---
+++ array/problem1.js
@@ -0,0 +1,6 @@
+let a=[4,5,6,7,8,9];
+
+for(let i=0;i<a.length-1;i++){
+  console.log((i),a[i]);
+ 
+}
---
+++ array/problem3.js
@@ -0,0 +1,8 @@
+// Given an array of numbers find the average of all the even numbers.
+let num=[4,5,6,7,8,9];
+let sum=0;
+for (let i=0;i<num.length;i++){
+  sum=sum+num[i];
+  
+}
+console.log(sum/num.length);
